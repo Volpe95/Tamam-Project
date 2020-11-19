cdbfb2428f0f1a1f23c7da391bebd198c9ead2fa
Mahmoud Kassem <m_m_kassem@hotmail.com>
2020-11-19 15:52:43 +0200

    system started to have pdf reports

--- src/app/comparisons/comparion-records/comparion-records.component.ts
+++ src/app/comparisons/comparion-records/comparion-records.component.ts
@@ -3,12 +3,21 @@ import { Subscription } from 'rxjs';
 import { ComponentCommunication } from 'src/app/shared/ComponentCommunication.service';
 import { utils } from 'src/utils/utils.service';
 
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+
 @Component({
   selector: 'app-comparion-records',
   templateUrl: './comparion-records.component.html',
   styleUrls: ['./comparion-records.component.css']
 })
 export class ComparionRecordsComponent implements OnInit {
+
+  fonts = {
+    Amiri:{
+      normal: "Amiri-Regular.ttf",
+    }
+  };
 
   subscription : Subscription;
   falseTamams : any[] = [] ;
@@ -26,7 +35,7 @@ export class ComparionRecordsComponent implements OnInit {
     ];
 
     ngOnInit(): void {
-
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
     this.subscription = this.sendMsg.falseTamamMsg.subscribe(message => {
 
       if(message === null){
@@ -54,5 +63,108 @@ export class ComparionRecordsComponent implements OnInit {
 
 
   }
+
+  printResults(){
+    console.log('Hello');
+    console.log('This is the data to print' , this.falseTamams);
+
+    var headersInArabic = [
+      {text: 'م' , alignment: 'center'},
+      {text: 'العسكري الرقم' , alignment: 'center'},
+      {text: 'الأسم' , alignment: 'center'},
+      {text: 'الفرقة' , alignment: 'center'},
+      {text: 'الأولى المحاضرة' , alignment: 'center'},
+      {text: 'الثانية المحاضرة' , alignment: 'center'},
+      {text: 'الثالثة المحاضرة' , alignment: 'center'},
+      {text: 'الرابعة المحاضرة' , alignment: 'center'},
+      {text: 'الخامسة المحاضرة' , alignment: 'center'},
+      {text: 'الطلبة لواء تمام' , alignment: 'center'},
+      {text: 'المحاضرات غياب عدد' , alignment: 'center'},
+    ];
+    var title = this.reverseString('تخلفات الطلبة بالمحاضرات بتاريخ ' + this.convertEnToAr(this.falseTamams[0]['date']).split('').reverse().join(''));
+    var dd = {
+      pageMargins: 10,
+      pageOrientation: 'landscape',
+      content: [
+          { text: title , style: 'header' , alignment: 'center'},
+          this.table(this.falseTamams , headersInArabic),
+      ],
+      defaultStyle: {
+        font: 'Amiri'
+      }
+
+    }
+    console.log(dd);
+    pdfMake.createPdf(dd , null , this.fonts).download();
+
+  }
+
+  reverseString(s){
+    return s.split(' ').reverse().join(" ");
+  }
+
+
+   buildTableBody(data, headersInArabic) {
+
+    function reverseString(s){
+      return s.split(' ').reverse().join(" ");
+    }
+
+    var body = [];
+
+    body.push(headersInArabic.reverse());
+
+    var columnsInEnglish = [
+      'studentID',
+      'studentName',
+      'Class',
+      'firstLectureTamam',
+      'secondLectureTamam',
+      'thirdLectureTamam',
+      'fourthLectureTamam',
+      'fifthLectureTamam',
+      'trueLewa2TalbaTamams',
+      'absenceCnt',
+    ];
+    var cnt = 1 ;
+    var convertToArabic = this.convertEnToAr;
+    data.forEach(function(row) {
+        var dataRow = [{text: cnt++ , alignment: 'center'}];
+
+        columnsInEnglish.forEach(function(column) {
+          var txt: any ;
+          if(column == 'Class')
+            txt = reverseString(convertToArabic(row['year'])
+            + row['className']
+            +(row['classNo'] != 0 ? convertToArabic(row['classNo']).split('').reverse().join(''): ''));
+
+          //else if(column == 'studentID')
+           // txt = convertToArabic(row[column].toString()).split('').reverse().join('');
+          else
+            txt = reverseString(row[column].toString());
+
+          dataRow.push({text: txt , alignment: 'center'});
+        })
+
+        body.push(dataRow.reverse());
+    });
+
+    return body;
+  }
+
+  table(data, columns) {
+      return {
+          table: {
+              widths: 'auto',
+              headerRows: 1,
+              body: this.buildTableBody(data, columns),
+              alignment: 'center',
+              pageBreakBefore: function(currentNode, followingNodesOnPage, nodesOnNextPage, previousNodesOnPage) {
+                return currentNode.headlineLevel === 1 && followingNodesOnPage.length === 0;
+             }
+          }
+      };
+  }
+
 
 }
--- src/app/manage-students/manage-students.component.ts
+++ src/app/manage-students/manage-students.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { jsPDF } from "jspdf";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import html2canvas from 'html2canvas';
+import autoTable from 'jspdf-autotable';
+import html2pdf from 'html2pdf';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-manage-students',
@@ -6,10 +13,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-students.component.css']
 })
 export class ManageStudentsComponent implements OnInit {
-
   constructor() { }
 
+  fonts = {
+    Amiri:{
+      normal: "Amiri-Regular.ttf",
+    }
+  };
   ngOnInit(): void {
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    var text = "سلام";
+    var docDefinition = {
+      content: text ,
+      defaultStyle: {
+      font: 'Amiri'
+    }
+  };
+  var externalDataRetrievedFromServer = [
+    { الاسم: 'sssssssssssssssssssssssssssssssssssssssssssssssssss', السن: 34 },
+    { الاسم: 'John', السن: 27 },
+    { الاسم: 'Elizabeth', السن: 30 },
+];
+
+function buildTableBody(data, columns) {
+    var body = [];
+
+    body.push(columns);
+
+    data.forEach(function(row) {
+        var dataRow = [];
+
+        columns.forEach(function(column) {
+            dataRow.push({text: row[column].toString() , alignment: 'center'});
+        })
+
+        body.push(dataRow);
+    });
+
+    return body;
+}
+
+function table(data, columns) {
+    return {
+        table: {
+            widths: ['*' , '*'],
+            headerRows: 1,
+            body: buildTableBody(data, columns),
+            alignment: 'center',
+        }
+    };
+}
+
+var dd = {
+    content: [
+        { text: 'Dynamic parts', style: 'header'},
+        table(externalDataRetrievedFromServer, ['الاسم', 'السن'])
+    ],
+    defaultStyle: {
+      font: 'Amiri'
+    }
+
+  }
+
+  pdfMake.createPdf(dd , null , this.fonts).download();
+
   }
 
 }
